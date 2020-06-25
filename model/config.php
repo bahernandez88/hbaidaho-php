c3a652cb5a456f4139b774913a890d7b081bab25
bhernandez-mycwi <55470962+bhernandez-mycwi@users.noreply.github.com>
2020-06-25 11:19:00 -0600

    Update config.php

--- model/config.php
+++ model/config.php
@@ -1,10 +1,9 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'pdb43.awardspace.net');
-define('DB_USERNAME', '3249449_applications');
-define('DB_PASSWORD', 'JulyFourteen88**');
-define('DB_NAME', '3249449_applications');
+
+
+/* Database information removed */
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
